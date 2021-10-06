9b296152f2c682a5aa6e156cae0c9e9dd8488569
Bianca Monteiro Corrêa <biancamonteiro22.06@gmail.com>
2021-10-05 22:47:43 -0300

    login, getUser e create feitos

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+import { PassportModule } from '@nestjs/passport';
+import { jwtConstants } from './jwt.constants';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, PrismaService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
